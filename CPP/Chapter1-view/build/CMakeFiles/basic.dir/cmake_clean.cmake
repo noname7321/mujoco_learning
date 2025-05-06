8649c5956a2b1d4226095f67534fd53f530343b0
albusgive <mb18743920955@163.com>
2025-05-06 21:34:03 +0800

    新文件：   CPP/Chapter1-view/CMakeLists.txt 	新文件：   CPP/Chapter1-view/basic.cc 	新文件：   CPP/Chapter1-view/build/CMakeCache.txt 	新文件：   CPP/Chapter1-view/build/CMakeFiles/3.22.1/CMakeCCompiler.cmake 	新文件：   CPP/Chapter1-view/build/CMakeFiles/3.22.1/CMakeCXXCompiler.cmake 	新文件：   CPP/Chapter1-view/build/CMakeFiles/3.22.1/CMakeDetermineCompilerABI_C.bin 	新文件：   CPP/Chapter1-view/build/CMakeFiles/3.22.1/CMakeDetermineCompilerABI_CXX.bin 	新文件：   CPP/Chapter1-view/build/CMakeFiles/3.22.1/CMakeSystem.cmake 	新文件：   CPP/Chapter1-view/build/CMakeFiles/3.22.1/CompilerIdC/CMakeCCompilerId.c 	新文件：   CPP/Chapter1-view/build/CMakeFiles/3.22.1/CompilerIdC/a.out 	新文件：   CPP/Chapter1-view/build/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp 	新文件：   CPP/Chapter1-view/build/CMakeFiles/3.22.1/CompilerIdCXX/a.out 	新文件：   CPP/Chapter1-view/build/CMakeFiles/CMakeDirectoryInformation.cmake 	新文件：   CPP/Chapter1-view/build/CMakeFiles/CMakeOutput.log 	新文件：   CPP/Chapter1-view/build/CMakeFiles/Makefile.cmake 	新文件：   CPP/Chapter1-view/build/CMakeFiles/Makefile2 	新文件：   CPP/Chapter1-view/build/CMakeFiles/TargetDirectories.txt 	新文件：   CPP/Chapter1-view/build/CMakeFiles/basic.dir/DependInfo.cmake 	新文件：   CPP/Chapter1-view/build/CMakeFiles/basic.dir/basic.cc.o 	新文件：   CPP/Chapter1-view/build/CMakeFiles/basic.dir/basic.cc.o.d 	新文件：   CPP/Chapter1-view/build/CMakeFiles/basic.dir/build.make 	新文件：   CPP/Chapter1-view/build/CMakeFiles/basic.dir/cmake_clean.cmake 	新文件：   CPP/Chapter1-view/build/CMakeFiles/basic.dir/compiler_depend.make 	新文件：   CPP/Chapter1-view/build/CMakeFiles/basic.dir/compiler_depend.ts 	新文件：   CPP/Chapter1-view/build/CMakeFiles/basic.dir/depend.make 	新文件：   CPP/Chapter1-view/build/CMakeFiles/basic.dir/flags.make 	新文件：   CPP/Chapter1-view/build/CMakeFiles/basic.dir/link.txt 	新文件：   CPP/Chapter1-view/build/CMakeFiles/basic.dir/progress.make 	新文件：   CPP/Chapter1-view/build/CMakeFiles/cmake.check_cache 	新文件：   CPP/Chapter1-view/build/CMakeFiles/progress.marks 	新文件：   CPP/Chapter1-view/build/Makefile 	新文件：   CPP/Chapter1-view/build/basic 	新文件：   CPP/Chapter1-view/build/cmake_install.cmake 	新文件：   CPP/Chapter1-view/tutorial.md 	修改：     MJCF/Chapter15-keyframe/scene.xml 	新文件：   Python/Chapter1-view/tutorial.md 	新文件：   Python/Chapter1-view/view.py

---
+++ CPP/Chapter1-view/build/CMakeFiles/basic.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basic.dir/basic.cc.o"
+  "CMakeFiles/basic.dir/basic.cc.o.d"
+  "basic"
+  "basic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
